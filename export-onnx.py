aa4223d00d95158b5d41dd00406493b845683ba7
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-27 16:47:45 +0700

    add sigmoid 50 to the outputs

--- export-onnx.py
+++ export-onnx.py
@@ -1,6 +1,18 @@
 import torch
+from torch import nn
 from dbnet import model_dbnet
 from dbnet.training import train
+
+
+class WithSigmoid(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x):
+        x, _, _ = self.model(x)
+        return torch.sigmoid(x * 50)
+
 
 image_size = (1024, 1024)
 hidden_size = 256
@@ -11,6 +23,7 @@ output_file = "db_mobilenet_v3_large.onnx"
 
 model = model_dbnet.DBNet(backbone, hidden_size, num_classes)
 model.load_state_dict(torch.load(weight, map_location="cpu"))
+model = WithSigmoid(model)
 model.eval()
 
 inputs = torch.rand(1, 3, *image_size)
