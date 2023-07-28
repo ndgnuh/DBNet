db570fd06cf52ff405354e5ca19dd509a118bd3f
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-28 09:59:06 +0700

    add save option

--- gen_config.py
+++ gen_config.py
@@ -1,3 +1,14 @@
+from argparse import ArgumentParser
 from dbnet.configs import example_config
 
-print(example_config())
+parser = ArgumentParser()
+parser.add_argument("-o", dest="output")
+args = parser.parse_args()
+config_string = example_config()
+
+if args.output is not None:
+    with open(args.output, "w") as fp:
+        fp.write(config_string)
+    print(f"Config written to {args.output}")
+else:
+    print(config_string)
